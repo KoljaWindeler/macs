2c4131a44933f59f4459db84006274c157f1e2c2
Kolja EC2 <KOlja.Windeler@gmail.com>
2015-10-03 18:45:04 -0500

    login alpha 0.1

---
+++ website/sec.php
@@ -0,0 +1,37 @@
+<?php
+include_once("con.php");
+if(isset($_GET["logout"])){
+	$_SESSION['ID']=0;
+	show_info("bye bye");
+};
+
+$login=0;
+
+if(!isset($_SESSION['ID']) or $_SESSION['ID']==0){
+	if(isset($_POST["macs_login"]) and isset($_POST["macs_pw"])){
+		$stmt = $db->prepare("SELECT `hash`,`id` FROM `macs`.`user` where login=:login");
+	        $stmt->bindParam(":login",$_POST["macs_login"],PDO::PARAM_STR);
+        	$stmt->execute();
+
+		foreach($stmt as $row){
+			//echo "given ".$_POST["macs_pw_md5"]." vs ".$row["hash"];
+			if($row["hash"]==$_POST["macs_pw_md5"]){
+				$_SESSION['ID']=$row["id"];
+				$login=1;
+				show_info("Good to see you again, ".$_POST["macs_login"]);
+				break;
+			}
+		};
+	}
+
+	if(!$login){
+		include_once("html.php");
+		$o='<form action="index.php?" method="post" id="login_form">';
+		$o.='<table class="logintable"><tr><td colspan="2">Please log in, use Peter / tinkerbell for now</td></tr>';
+		$o.='<tr><td><input type="text" id="macs_login" name="macs_login"></td><td><input type="password" id="macs_pw" name="macs_pw"></td></tr>';
+		$o.='<tr><td colspan="2"><input type="hidden" name="macs_pw_md5" id="macs_pw_md5"><input type="submit"></td></tr></table></form>';
+		echo $header.$o.$footer;
+		exit(0);
+	};
+};
+?>
