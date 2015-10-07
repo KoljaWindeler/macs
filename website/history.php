7a0c723ea7db1438ca10a87edb03ab488f1d7246
Kolja EC2 <KOlja.Windeler@gmail.com>
2015-10-07 15:58:38 -0500

    added usage

--- website/history.php
+++ website/history.php
@@ -9,6 +9,11 @@ require_once('con.php');
 if(isset($_GET["logme"]) && isset($_GET["event"]) && (isset($_GET["mach_nr"]) || isset($_GET["badge"]))){
 	$event=$_GET["event"];
 
+	if(isset($_GET["timeopen"])){
+		$time=$_GET["timeopen"];
+	} else {
+		$time="-";
+	};
 
 	if(!isset($_GET["mach_nr"])){
 		$mach="-";
@@ -35,18 +40,18 @@ if(isset($_GET["logme"]) && isset($_GET["event"]) && (isset($_GET["mach_nr"]) ||
 			$user="-";
 		};
 	}
-	add_log($mach,$user,$event);
+	add_log($mach,$user,$event,$time);
 };
 
-
-function add_log($machine,$user,$event){
+function add_log($machine,$user,$event,$time){
 	global $db;
 	//echo "adding ".$machine." ".$user." ".$event;
-	$stmt = $db->prepare("INSERT INTO  `macs`.`log` (`timestamp`,`user_id`,`machine_id`,`event`,`login_id`) VALUE (".time().",:user,:machine,:event,:login)");
+	$stmt = $db->prepare("INSERT INTO  `macs`.`log` (`timestamp`,`user_id`,`machine_id`,`event`,`login_id`,`usage`) VALUE (".time().",:user,:machine,:event,:login,:time)");
 	$stmt->bindParam(":user",$user,PDO::PARAM_STR);
 	$stmt->bindParam(":machine",$machine,PDO::PARAM_STR);
 	$stmt->bindParam(":event",$event,PDO::PARAM_STR);
 	$stmt->bindParam(":login",$_SESSION['ID'],PDO::PARAM_INT);
+	$stmt->bindParam(":time",$time,PDO::PARAM_INT);
 	$stmt->execute();
 };
 
