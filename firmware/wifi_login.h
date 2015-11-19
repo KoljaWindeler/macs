77dc3b10259831802fa9c730ceaf1529ccc6e8c0
Kolja EC2 <KOlja.Windeler@gmail.com>
2015-11-18 21:26:25 -0600

    added read wifi login from serial, from EEPROM and save and validate data

--- firmware/wifi_login.h
+++ firmware/wifi_login.h
@@ -1,2 +1,8 @@
+#include "application.h"
+#include "stdint.h"
+
 void set_update_login();
 void set_macs_login();
+bool set_wifi_config(uint8_t id);
+bool check_wifi_config(String SSID,String pw,uint8_t type,uint8_t chk);
+bool parse_wifi();
