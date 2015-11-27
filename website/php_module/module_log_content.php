da055dd1fd393e541c636214cee2df6209145860
kolja ubuntu <kolja.windeler@gmail.com>
2015-11-26 19:36:51 -0600

    avoid reading var that does not exists

--- website/php_module/module_log_content.php
+++ website/php_module/module_log_content.php
@@ -105,10 +105,11 @@ foreach ($stmt as $row) {
 	$u_out=str_replace(";",",",$u_out);
 	$extra=str_replace(";",",",$extra);
 
-	if(is_numeric($_SESSION['tz'])){
-		$tz=$_SESSION['tz'];
-	} else {
-		$tz=0;
+	$tz=0;
+	if(isset($_SESSION['tz'])){
+		if(is_numeric($_SESSION['tz'])){
+			$tz=$_SESSION['tz'];
+		};
 	};
 
  	$o_log.=date("Y/m/d H:i",$row["timestamp"]-$tz).';'.$row["event"].';'.$m_out.';'.$u_out.';'.$extra."\r\n";
